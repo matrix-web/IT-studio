f18fe8982a7fab25a81007197bf6c52001f3adef
Ivan Plastun <plastun.ivan2018@yandex.ru>
2020-12-13 18:41:25 +0300

    Added scripts for opening the sidebar on mobile devices using the swipe event

--- src/js/modules/header-nav.js
+++ src/js/modules/header-nav.js
@@ -1,11 +1,8 @@
 // Mobile nav active
-
 const buttonMobileNav = document.querySelector('.header__nav-mobile')
 const headerNav = document.querySelector('.header-nav')
 
 buttonMobileNav.addEventListener('click', () => {
-    console.log('click')
     buttonMobileNav.classList.toggle('header__nav-mobile--active')
     headerNav.classList.toggle('header-nav--open')
-
 })
